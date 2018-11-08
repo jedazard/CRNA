1de37691399f83967b86a7f993a1d39262944191
zzaazzaa <32473855+zzaazzaa@users.noreply.github.com>
2018-11-07 19:06:52 -0500

    Update CRNA.R

--- GSE11352/CRNA.R
+++ GSE11352/CRNA.R
@@ -74,7 +74,7 @@ omnipath <- read.csv("data_for_markdown/omnipath_final.csv",row.names = 1)
 GSE11352_DEGList <- read.csv("data_for_markdown/GSE11352_GSE11352_DEGList.csv",row.names = 1)
 GSE11352_exprs_for_JTREE <- read.csv("data_for_markdown/GSE11352_scaled_exprs.csv",row.names = 1)[,-1]
 omniNet <- graph_from_data_frame(d = omnipath,directed = T)
-miarray <- as.data.frame(GSE11352_DEGList[which(abs(GSE11352_DEGList$logFC) >= 2),])
+miarray <- as.data.frame(DEGList[which(abs(DEGList$logFC) >= log2(2)),])
 
 #########################################################################################
 # RWR
